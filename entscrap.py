5228d48d71b0c7300354f8c1684f5a42834e89ac
Nicolas <77850354+NicolasVaillant@users.noreply.github.com>
2022-04-15 20:52:03 +0200

    Update entscrap.py

--- entscrap.py
+++ entscrap.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from email import message
 from re import M
 import scrapy
 from scrapy.crawler import CrawlerProcess
@@ -7,6 +8,9 @@ import unicodedata
 import numpy as np
 import smtplib
 from lxml import etree
+import smtplib
+from email.message import EmailMessage
+
 
 def authentication_failed(response):
     pass
@@ -20,29 +24,49 @@ def sendMail(FROM, TO, sender, mdp, subject, link, notes):
     print("Sending mail...")
     SUBJECT = "Nouvelle(s) note(s) disponible(s) sur l'ENT"
     
-    TEXT = "Mail automatique - merci de ne pas y repondre.\n\n"
-    MSG = """Une nouvelle note est disponible sur l'ENT."""
+    TEXT = "Mail automatique - merci de ne pas y repondre.<br><br>"
 
-    FOOTER = """\nConsulter votre note via le lien suivant (authentification requise) : {}.""".format(link)
+    FOOTER = """<br>Consulter votre note via le lien suivant (authentification requise) : <a style="color: #5a5ad0;font-weight: bold;">{}</a>.""".format(link)
 
     content = ""
     for word in notes:
         e = word.split(',')
-        lines = """Date : {}\nCours : {}\nIntervenant : {}\nNote : {}\n""".format(e[0], e[1], e[2], e[3])
+        lines = """
+        <div style="padding:.5em;margin-bottom:.5em;background-color:#eee;font-family: 'Helvetica', 'Roboto', sans-serif;width:100%;border-radius: 5px;">
+            <div style="font-family : 'Helvetica','Roboto', sans-serif;">Date : {}</div>
+            <div style="font-family : 'Helvetica', 'Roboto', sans-serif;">Cours : {}</div>
+            <div style="font-family : 'Helvetica', 'Roboto', sans-serif;">Intervenant : {}</div>
+            <div style="color: #5a5ad0;font-weight: bold;">Note : {}</div>
+        </div>
+        """.format(e[0], e[1], e[2], e[3])
 
-        content += str(lines) + "------\n"
+        content += str(lines)
 
-    msg = TEXT + content + FOOTER
-    
-    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
-    """ % (FROM, ", ".join(TO), SUBJECT, msg)
+    message = EmailMessage()
+    message['Subject'] = 'Nouvelle(s) note(s) disponible(s) sur l\'ENT'
+    message['From'] = FROM 
+    message['To'] = TO 
+    message.set_content('See below')
+
+    contenu = """
+    <!DOCTYPE html>
+    <html>
+        <body style="font-family:'Helvetica','Roboto', sans-serif;">
+            <div style="width:100%;height:auto;color: #5a5ad0;font-weight: bold;">{}</div>
+            <div style="width:100%;height:auto; padding:1em 0">{}</div>
+            <div style="width:100%;height:auto;">{}</div>
+        </body>
+    </html>
+    """.format(TEXT, content, FOOTER)
+
+    message.set_content(contenu, subtype='html')
 
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.ehlo()
         server.starttls()
         server.login(sender, mdp)
-        server.sendmail(FROM, TO, message)
+        server.send_message(message)
         server.close()
         print('successfully sent the mail')
     except smtplib.SMTPResponseException as e:
@@ -80,15 +104,12 @@ class entSpider(scrapy.Spider):
             headers = [col.text for col in next(rows)]
             for row in rows:
                 values = [col.text for col in row]
-                # use list(result) if needed
-                result = zip(headers, values)
                 notesEnt.append(values)
         
 
             values = ','.join(str(v) for v in notesEnt)
             valuesNA = ''.join((c for c in unicodedata.normalize('NFD', values) if unicodedata.category(c) != 'Mn'))
             
-            arrayValuesNA = ''.join(valuesNA)
             arrayNote = 'note.json'
 
             with open(arrayNote, 'w') as fNP:
@@ -99,18 +120,17 @@ class entSpider(scrapy.Spider):
             from_ent = np.array(valuesNA)
             from_array = np.array(f.read()[1:-1])
 
+            print(valuesNA)
+            print(len(valuesNA))
             all = []
-            MSG = ""
             for note in notesEnt : 
                 print(note)
                 notesE = ','.join(str(v) for v in note)
                 notesNA = ''.join((c for c in unicodedata.normalize('NFD', notesE) if unicodedata.category(c) != 'Mn'))
-
-                all.append(notesNA)      
+                all.append(notesNA)
+    
 
             if((from_ent == from_array).all() == True):
-                # sendMail(FROM, TO, sender, mdp, subject, link, UE, date, note):
-                print("Sending mail")
                 sendMail(
                     stmp_sender, 
                     stmp_receiver if isinstance(stmp_receiver, list) else [stmp_receiver],
